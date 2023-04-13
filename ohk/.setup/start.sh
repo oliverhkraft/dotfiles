51f80d3704384b87f2e3b6ebb858cd439da4a7c7
Oliver Hertzman Kraft <oliver@oliverkraft.io>
2023-04-13 15:09:25 +0200

    Updated zsh fixes

--- ohk/.setup/start.sh
+++ ohk/.setup/start.sh
@@ -23,11 +23,10 @@ ansible-playbook ohk/.setup/playbook.yml
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
-p10k configure
-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+pretty_print p10k configure
 
 pretty_print "Done! 🎉"
