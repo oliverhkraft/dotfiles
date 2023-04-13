11b336207f6379396f4f6d2a1f5a8069e3b6de8f
Oliver Hertzman Kraft <oliver@oliverkraft.io>
2023-04-13 13:34:12 +0200

    Updated brew path

--- ohk/.setup/start.sh
+++ ohk/.setup/start.sh
@@ -11,14 +11,7 @@ pretty_print "Here we go..."
 # Homebrew installation
 if ! command -v brew &>/dev/null; then
   pretty_print "Installing Homebrew, an OSX package manager, follow the instructions..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-  if ! grep -qs "recommended by brew doctor" ~/.zshrc; then
-    pretty_print "Put Homebrew location earlier in PATH ..."
-      printf '\n# recommended by brew doctor\n' >> ~/.zshrc
-      printf 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
-      export PATH="/usr/local/bin:$PATH"
-  fi
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   pretty_print "You already have Homebrew installed...good job!"
 fi
